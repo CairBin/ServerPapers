8b35425e46bad2a0d7dc0ff88cd3cc28d96fbc35
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-12-01 03:11:06 +0800

    fix: Fix the bug where pm2 does not use the tsconfig. json of the project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "ServerPapers",
       script: "./src/App.ts",
       interpreter: "./node_modules/.bin/ts-node",
+      interpreter_args: '-r ts-node/register -r tsconfig-paths/register',
       exec_mode: "cluster",
     },
   ],
